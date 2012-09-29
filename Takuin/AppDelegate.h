f7dd4e43bc6f55ac8da0d704f4485fccfa4f7e2d
Miro Jelicic <mjelicic@ebay.com>
2012-09-29 20:03:40 +0200

    First view using storyboard

--- Takuin/AppDelegate.h
+++ Takuin/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  Takuin
-//
-//  Created by Miro Jelicic on 9/29/12.
-//  Copyright (c) 2012 Independent Trading. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
